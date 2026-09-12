7aee7e94b11eedab2435ba6851de34a054d90024
Abhinandan Kumar <Abhinandankumar-ui@users.noreply.github.com>
2026-09-12 19:55:56 +0530

    connect frontend to backend: VITE_API_URL https://smcclub-1.onrender.com

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import axios from 'axios'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://smcclub-1.onrender.com'
 ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>)
